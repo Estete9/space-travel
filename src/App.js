6b43670212dae3fced3cdc1aadd27cf1799baa65
Esteban <cepalacios@outlook.com>
2023-10-10 17:35:57 -0500

    Create empty My profile - Setup with NavLink

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import {
+  BrowserRouter as Router, Routes, Route, Outlet,
+} from 'react-router-dom';
 import './App.css';
+import MyPage from './components/MyPage';
+
 import Header from './components/Header';
+
+function Layout() {
+  return <Outlet />;
+}
 
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
-
+    <Router>
+      <div id="wrapper">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<MyPage />} />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import Logo from '../assets/planet.png';
 
-const Header = () => (
-  <div>
-    <header>
-      <nav>
-        <img src={Logo} alt="Logo" />
-        <ul>
-          <li><a href="/">Rockets</a></li>
-          <li><a href="/">Missions</a></li>
-          <li><a href="/">My Profile</a></li>
-        </ul>
-      </nav>
-    </header>
+const Header = () => {
+  const links = [
+    { path: '/', text: 'Rockets' },
+    { path: 'missions', text: 'Missions' },
+    { path: 'profile', text: 'My Profile' },
+  ];
 
-  </div>
-);
+  return (
+    <div>
+      <header>
+        <nav>
+          <img src={Logo} alt="Logo" />
+          <ul>
+            {links.map((link) => (
+              <li key={link.text.toLowerCase()}>
+                <NavLink to={link.path}>{link.text}</NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+};
 
 export default Header;
