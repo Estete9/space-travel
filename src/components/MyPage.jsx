c268bad45e2398d3e062771f2b876fceb05021de
maximoortelli <maximoortelli@hotmail.com>
2023-10-12 16:44:06 -0300

    Add files
    Update MyPage.jsx
    Add myProfile.css
    Update missionsSlice.js
    Update Missions.jsx
    Update MissionsList.jsx

--- src/components/MyPage.jsx
+++ src/components/MyPage.jsx
@@ -1,5 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import '../styles/myProfile.css';
+import { fetchMissions } from '../redux/missions/missionsSlice';
 
-const MyPage = () => <div>MyPage</div>;
+const MyProfile = () => {
+  const dispatch = useDispatch();
+  const [loaded, setLoaded] = useState(false);
+  const { missions } = useSelector((state) => state.missions);
+  const activeMissions = missions.filter((active) => active.reserved);
 
-export default MyPage;
+  useEffect(() => {
+    if (missions.length === 0) {
+      dispatch(fetchMissions());
+    } else {
+      setLoaded(true);
+    }
+  }, [dispatch, missions]);
+
+  if (!loaded) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <section className="myProfile">
+      <div style={{ textAlign: 'left' }}>
+        <h3 style={{ marginBottom: '10px' }}>My Missions</h3>
+        <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+          <tbody>
+            {activeMissions.map((missions) => (
+              <tr key={missions.mission_id}>
+                <td style={{ border: '1px solid #dedede', padding: '23px', backgroundColor: 'white' }}>{missions.mission_name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+};
+
+export default MyProfile;
